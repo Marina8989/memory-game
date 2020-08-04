cb96a2f2f4ab4acab59b5de7957b2915e1face51
Marina8989 <marina.mill209@gmail.com>
2020-08-03 18:38:22 -0700

    working on js part

--- script.js
+++ script.js
@@ -1,41 +1,24 @@
 const gameContainer = document.getElementById("game");
 
-const COLORS = [
-  "red",
-  "blue",
-  "green",
-  "orange",
-  "purple",
-  "red",
-  "blue",
-  "green",
-  "orange",
-  "purple"
-];
-
+const COLORS = ["red", "blue", "green","orange", "purple", "red", "blue", "green", "orange", "purple"];
 // here is a helper function to shuffle an array
 // it returns the same array with values shuffled
 // it is based on an algorithm called Fisher Yates if you want ot research more
 function shuffle(array) {
   let counter = array.length;
-
   // While there are elements in the array
   while (counter > 0) {
     // Pick a random index
     let index = Math.floor(Math.random() * counter);
-
     // Decrease counter by 1
     counter--;
-
     // And swap the last element with it
     let temp = array[counter];
     array[counter] = array[index];
     array[index] = temp;
   }
-
   return array;
 }
-
 let shuffledColors = shuffle(COLORS);
 
 // this function loops over the array of colors
@@ -45,13 +28,10 @@ function createDivsForColors(colorArray) {
   for (let color of colorArray) {
     // create a new div
     const newDiv = document.createElement("div");
-
     // give it a class attribute for the value we are looping over
     newDiv.classList.add(color);
-
     // call a function handleCardClick when a div is clicked on
     newDiv.addEventListener("click", handleCardClick);
-
     // append the div to the element with an id of game
     gameContainer.append(newDiv);
   }
@@ -61,14 +41,14 @@ function createDivsForColors(colorArray) {
 function handleCardClick(event) {
   // you can use event.target to see which element was clicked
   console.log("you just clicked", event.target);
-
-      event.target.style.background = event.target.className;
-   
-    setTimeout(() => {
-        event.target.style.background = 'none';
-    }, 1000);
- 
-}
+  }
 
 // when the DOM loads
 createDivsForColors(shuffledColors);
+
+
+
+// Clicking a card should change the background color to be the color of the class it has.
+// Users should only be able to change at most two cards at a time.
+// Clicking on two matching cards should be a “match” — those cards should stay face up.
+// When clicking two cards that are not a match, they should stay turned over for at least 1 second before they hide the color again.You should make sure to use a setTimeout so that you can execute code after one second.
